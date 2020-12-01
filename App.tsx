39631d7d60e96f9c27710ee4d416cb31e8c6c3a2
RafaelGoulartB <rafagoulartb@gmail.com>
2020-12-01 09:00:32 -0300

    feat: add routes component

--- App.tsx
+++ App.tsx
@@ -1,14 +1,12 @@
 import React from 'react'
-import { StatusBar } from 'expo-status-bar'
 import { Provider as PaperProvider } from 'react-native-paper'
 import { theme } from './src/styles/theme'
-import { Text } from 'react-native'
+import Routes from './src/routes'
 
 export default function App() {
   return (
     <PaperProvider theme={theme}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Routes />
     </PaperProvider>
   )
 }

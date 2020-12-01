29a84ab6c2f66577f70e29366c4f9bb8c1c2ae7b
RafaelGoulartB <rafagoulartb@gmail.com>
2020-12-01 08:55:04 -0300

    feat: add react-native-paper provider

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
-import { StatusBar } from 'expo-status-bar'
 import React from 'react'
-import { Text, View } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+import { Provider as PaperProvider } from 'react-native-paper'
+import { theme } from './src/styles/theme'
+import { Text } from 'react-native'
 
 export default function App() {
   return (
-    <View>
+    <PaperProvider theme={theme}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+    </PaperProvider>
   )
 }

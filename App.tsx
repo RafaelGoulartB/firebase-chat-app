8fc0a999b8cd8b299b17531b989e7d43866096d5
RafaelGoulartB <rafagoulartb@gmail.com>
2020-12-01 09:19:41 -0300

    feat: add navigation container

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
 import { Provider as PaperProvider } from 'react-native-paper'
 import { theme } from './src/styles/theme'
 import Routes from './src/routes'
+import { StatusBar } from 'expo-status-bar'
 
 export default function App() {
   return (
     <PaperProvider theme={theme}>
-      <Routes />
+      <NavigationContainer>
+        <Routes />
+      </NavigationContainer>
+      <StatusBar style="dark" />
     </PaperProvider>
   )
 }

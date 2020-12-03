7150cfdbf98eee60c94aa089351f302ab44de93c
RafaelGoulartB <rafagoulartb@gmail.com>
2020-12-03 10:02:19 -0300

    feat: add home layout

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { List } from 'react-native-paper'
+import ChatItem from '../../components/chat-item'
+import { ChatsItemFixures } from './constants'
 
 const Home: React.FC = () => {
-  return <Text>Home</Text>
+  return (
+    <List.Section>
+      <List.Subheader>Chats</List.Subheader>
+      {ChatsItemFixures.map(item => (
+        <ChatItem
+          key={item.id}
+          id={item.id}
+          title={item.title}
+          imageSrc={item.imageSrc}
+        />
+      ))}
+    </List.Section>
+  )
 }
 
 export default Home

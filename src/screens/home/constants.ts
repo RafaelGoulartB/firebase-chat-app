f9ca6aa7e961688ffa7a3e0713029c748f5855d5
RafaelGoulartB <rafagoulartb@gmail.com>
2020-12-03 10:02:49 -0300

    feat: add chats item fixures to home screen

---
+++ src/screens/home/constants.ts
@@ -0,0 +1,14 @@
+interface ChatsItemInterface {
+  id: string
+  title: string
+  imageSrc: string
+}
+
+export const ChatsItemFixures: ChatsItemInterface[] = [
+  { id: '1', title: 'Ana', imageSrc: 'https://images.generated.photos/mdC04J0sVP2l9MmOBnZG_NxpCMIsKLN8FhmqwUnXKgI/rs:fit:512:512/wm:1:sowe:18:18:0.33/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzA0NTc3MzYuanBn.jpg' },
+  { id: '2', title: 'Rafael', imageSrc: 'https://images.generated.photos/WPNrtOGUijC5l3xVR3i2RUkKuQFs-9kr6Wvg9Vo1JeM/rs:fit:512:512/wm:1:sowe:18:18:0.33/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzA1NDIxMzguanBn.jpg' },
+  { id: '3', title: 'Amanda', imageSrc: 'https://images.generated.photos/-ZnwbOhYOSxLnDRmnTXrO01Fx5d-YvRkx6y1CGetj_0/rs:fit:512:512/wm:1:sowe:18:18:0.33/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzA5MTE5OTAuanBn.jpg' },
+  { id: '4', title: 'Gabriela', imageSrc: 'https://images.generated.photos/2sU4DRm_0b8T1VjG3-zTl4tAOMhOzb-Nd8qQlhN1kQc/rs:fit:512:512/wm:1:sowe:18:18:0.33/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzAwMDc4MjRfMDgy/OTE3MV8wODUxNzg1/LmpwZw.jpg' },
+  { id: '5', title: 'Julio', imageSrc: 'https://images.generated.photos/yC8y1X07UhJRXC6MpYIoGFY-SWsvpmyorUPV2nAizrU/rs:fit:512:512/wm:1:sowe:18:18:0.33/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzA5MjcxMzguanBn.jpg' }
+
+]

83772f34599d54ec17ac87122104376eb0ade1d7
RafaelGoulartB <rafagoulartb@gmail.com>
2020-12-03 09:00:21 -0300

    feat: customize stack navigator in routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,8 +7,20 @@ const Stack = createStackNavigator()
 
 const Routes: React.FC = () => {
   return (
-    <Stack.Navigator initialRouteName="Home">
-      <Stack.Screen name="Home" component={Home} />
+    <Stack.Navigator
+      initialRouteName="Chat"
+      screenOptions={{
+        headerTintColor: 'white',
+        headerStyle: { backgroundColor: '#208b82' }
+      }}
+    >
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{
+          title: 'Chats'
+        }}
+      />
       <Stack.Screen name="Chat" component={Chat} />
     </Stack.Navigator>
   )

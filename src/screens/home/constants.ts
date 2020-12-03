bbb5e83a17c22c9b5d035dd8d5f579d315f7d8b6
RafaelGoulartB <rafagoulartb@gmail.com>
2020-12-03 10:09:10 -0300

    feat: add more chats to fixures

--- src/screens/home/constants.ts
+++ src/screens/home/constants.ts
@@ -9,6 +9,8 @@ export const ChatsItemFixures: ChatsItemInterface[] = [
   { id: '2', title: 'Rafael', imageSrc: 'https://images.generated.photos/WPNrtOGUijC5l3xVR3i2RUkKuQFs-9kr6Wvg9Vo1JeM/rs:fit:512:512/wm:1:sowe:18:18:0.33/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzA1NDIxMzguanBn.jpg' },
   { id: '3', title: 'Amanda', imageSrc: 'https://images.generated.photos/-ZnwbOhYOSxLnDRmnTXrO01Fx5d-YvRkx6y1CGetj_0/rs:fit:512:512/wm:1:sowe:18:18:0.33/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzA5MTE5OTAuanBn.jpg' },
   { id: '4', title: 'Gabriela', imageSrc: 'https://images.generated.photos/2sU4DRm_0b8T1VjG3-zTl4tAOMhOzb-Nd8qQlhN1kQc/rs:fit:512:512/wm:1:sowe:18:18:0.33/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzAwMDc4MjRfMDgy/OTE3MV8wODUxNzg1/LmpwZw.jpg' },
-  { id: '5', title: 'Julio', imageSrc: 'https://images.generated.photos/yC8y1X07UhJRXC6MpYIoGFY-SWsvpmyorUPV2nAizrU/rs:fit:512:512/wm:1:sowe:18:18:0.33/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzA5MjcxMzguanBn.jpg' }
-
+  { id: '5', title: 'Julio', imageSrc: 'https://images.generated.photos/yC8y1X07UhJRXC6MpYIoGFY-SWsvpmyorUPV2nAizrU/rs:fit:512:512/wm:1:sowe:18:18:0.33/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzA5MjcxMzguanBn.jpg' },
+  { id: '6', title: 'Sergio', imageSrc: 'https://images.generated.photos/FK5N11DMrIr-jWFS3gTErCbYp6wXZEIhIyGdZCOe1gY/rs:fit:512:512/wm:1:sowe:18:18:0.33/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzA0MTM3MDNfMDUx/NzM0N18wMzU2NjE1/LmpwZw.jpg' },
+  { id: '7', title: 'Rafael G.', imageSrc: 'https://images.generated.photos/4nVgl1tPOOa8dYh4pGJkLOSam_ptbgjFTek5CnARny4/rs:fit:256:256/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzA3ODQyNzZfMDI0/MDg0NV8wODQwNzQ5/LmpwZw.jpg' },
+  { id: '8', title: 'Lucia', imageSrc: 'https://images.generated.photos/0c-vroUciU5UOBnaEWzq9FWAeXhQvPQXEBoupGPZbBc/rs:fit:512:512/wm:1:sowe:18:18:0.33/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzA4NDExNDIuanBn.jpg' }
 ]
